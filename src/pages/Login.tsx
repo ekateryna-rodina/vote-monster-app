f3fdca6e5d2e1334b1bacfb030eaae24542680a7
ekateryna-rodina <work.rodina@gmail.com>
2022-05-13 13:08:17 +0300

    disable logout function

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -31,9 +31,9 @@ const Login = () => {
       console.log(err);
     }
   };
-  const onDisconnect = () => {
-    dispatch(setIsConnected(false));
-  };
+  // const onDisconnect = () => {
+  //   dispatch(setIsConnected(false));
+  // };
 
   return (
     <div className="h-full flex justify-center items-center">
